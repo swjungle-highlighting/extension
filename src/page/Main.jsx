aa842f9775f1562445b815ce3622e0be70695dfe
DongGeun974 <zxcasd3004@naver.com>
2022-03-02 03:16:33 +0900

    axios

--- src/page/Main.jsx
+++ src/page/Main.jsx
@@ -1,15 +1,37 @@
 /*global chrome*/
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function Main(props) {
     
-    const serviceURL = 'localhost:3000';
+    const server_addr = "http://143.248.193.175:5000";
+    const [data, setData] = useState();
+
+    function requestResult(url) {
+        axios
+          .post(server_addr + "/flask/hello", {
+            url: url,
+          })
+          .then((response) => {
+            chrome.extension.getBackgroundPage().console.log('foo');
+            chrome.extension.getBackgroundPage().console.log(response.data);
+            setData(JSON.stringify(response.data.bookmarker))
+          })
+          .catch((error) => {
+          });
+      }
+      
+      useEffect(()=>{
+        requestResult(props.url)
+      },[])
 
     return (
         <div>
             {props.url}
-            메인입니다.
+            <div>
+                {data}
+            </div>
         </div>
     );
 }
