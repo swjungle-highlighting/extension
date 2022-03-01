5828737293e01e3e1f9cf90c14aceac24e3553f7
DongGeun974 <zxcasd3004@naver.com>
2022-03-01 23:09:30 +0900

    fetch current tab url

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
+/*global chrome*/
+
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [url, setUrl] = useState('');
+
+  /**
+   * Get current URL
+   */
+  useEffect(() => {
+      const queryInfo = {active: true, lastFocusedWindow: true};
+
+      chrome.tabs && chrome.tabs.query(queryInfo, tabs => {
+          const url = tabs[0].url;
+          setUrl(url);
+      });
+  }, []);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+          <header className="App-header">
+              <img src={logo} className="App-logo" alt="logo"/>
+              <p>URL:</p>
+              <p>
+                  {url}
+              </p>
+          </header>
+      </div>
   );
 }
 
