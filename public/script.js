791fad2d2cc33afbc9a965a89654d3617f6fa084
DongGeun974 <zxcasd3004@naver.com>
2022-03-08 15:07:57 +0900

    fix click

--- public/script.js
+++ public/script.js
@@ -4,9 +4,10 @@ function get_source(time_percent) {
 
     const div_elem = document.createElement("div");
     const textnode = document.createTextNode('★');
-    let style_attr = 'bottom: 11px; left: calc('+time_percent+'% - 8px); user-select: auto; position: absolute !important; width: 12px !important; height: 17px !important; padding-top: 30px !important; background-size: cover !important; background-position: bottom !important; background-repeat: no-repeat !important; cursor: pointer !important; z-index: 2000 !important;';
-    div_elem.setAttribute('style',style_attr);
+    let style_attr = 'bottom: 5px; left: calc(' + time_percent + '% - 8px); user-select: auto; position: absolute ; width: 12px ; height: 17px ; padding-top: 30px ; background-size: cover ; background-position: bottom ; background-repeat: no-repeat ; cursor: pointer ; z-index: 27 ;';
+    div_elem.setAttribute('style', style_attr);
     div_elem.appendChild(textnode);
+    div_elem.addEventListener('click', clickBack);
 
     var elements = document.getElementsByClassName('ytp-progress-bar-container')[0];
     elements.appendChild(div_elem);
@@ -14,6 +15,24 @@ function get_source(time_percent) {
 
     return time_percent;
 }
+
+function clickBack(event) {
+    console.log(event.x)
+    console.log(event.y)
+    
+    console.log(window)
+     var ev = new MouseEvent('click', {
+        'view': window,
+        'bubbles': true,
+        'cancelable': true,
+        'screenX': event.x,
+        'screenY': event.y
+    });
+    var el = document.elementFromPoint(event.x, event.y);
+    el.dispatchEvent(ev);
+
+}
+
 chrome.extension.sendMessage({
     action: "getSource",
     source: get_source(config)
